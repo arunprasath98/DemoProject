b528dcd09436910c8cb32244e0cf945c9a812b0a
arunprasath98 <arunmcekct1998@gmail.com>
2020-11-26 06:31:59 -0600

    proj

--- src/org/demo/Hithub.java
+++ src/org/demo/Hithub.java
@@ -1,5 +1,10 @@
 package org.demo;
 
 public class Hithub {
+	public static void main(String[] args) {
+		System.out.println("hgag");
+			System.out.println("hgag");
+			System.out.println("hgag");
+	}
 
 }
